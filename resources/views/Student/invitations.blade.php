eeba31e48e229aa049369d2411b868b2e33b6a2a
Javine Justy Fajardo <jade.fajardo.up@phinmaed.com>
2024-10-16 23:03:37 +0800

    https://github.com/PransLoren/Synchro.git

--- resources/views/Student/invitations.blade.php
+++ resources/views/Student/invitations.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Pending Project Invitations</h1>
+                    <h1 class="m-0">Project Report</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -19,45 +19,84 @@
             <div class="row">
                 <div class="col-md-12">
 
-                    @include('message')
+                    <!-- Card for Project Report -->
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Invitations List</h3>
+                            <h3 class="card-title">Project List</h3>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body p-0">
-                            @if($invitations->count() > 0)
-                                <table class="table table-striped" style="background-color: #edf2fb;">
-                                    <thead>
-                                        <tr>
-                                            <th>Project Name</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($invitations as $invitation)
-                                            <tr style="background-color: #eff8ff;">
-                                                <td>{{ $invitation->project->class_name }}</td>
-                                                <td>
-                                                    <form action="{{ route('projects.acceptInvitation', [$invitation->project->id]) }}" method="POST" style="display:inline;">
-                                                        @csrf
-                                                        <button type="submit" class="btn btn-success">Accept</button>
-                                                    </form>
-                                                    <form action="{{ route('projects.rejectInvitation', [$invitation->project->id]) }}" method="POST" style="display:inline;">
-                                                        @csrf
-                                                        <button type="submit" class="btn btn-danger">Reject</button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-
-                                    </tbody>
-                                </table>
-                            @else
-                                <p style="padding: 15px;">No Pending Invitations</p>
-                            @endif
+                            <table class="table table-hover table-striped" style="background-color: #edf2fb;">
+                                <thead style="background-color: #dde2e8;">
+                                    <tr>
+                                        <th>Project Name</th>
+                                        <th>Submission Date</th>
+                                        <th>Submission Time</th>
+                                        <th>Add Tasks</th>
+                                        <th>Action</th>
+                                        <th>Invite Users</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr style="background-color: #eff8ff;">
+                                        <td>Project ni Dos</td>
+                                        <td>2024-10-15</td>
+                                        <td>13:07:00</td>
+                                        <td>
+                                            <button class="btn btn-primary">
+                                                <i class="fas fa-plus"></i>
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-success">
+                                                <i class="fas fa-check"></i>
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-warning">Invite User</button>
+                                        </td>
+                                    </tr>
+                                    <tr style="background-color: #eff8ff;">
+                                        <td>Uno Project</td>
+                                        <td>2024-10-15</td>
+                                        <td>10:58:00</td>
+                                        <td>
+                                            <button class="btn btn-primary">
+                                                <i class="fas fa-plus"></i>
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-success">
+                                                <i class="fas fa-check"></i>
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-warning">Invite User</button>
+                                        </td>
+                                    </tr>
+                                    <tr style="background-color: #eff8ff;">
+                                        <td>DOS</td>
+                                        <td>2024-10-15</td>
+                                        <td>22:27:00</td>
+                                        <td>
+                                            <button class="btn btn-primary">
+                                                <i class="fas fa-plus"></i>
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-success">
+                                                <i class="fas fa-check"></i>
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-warning">Invite User</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
+                    <!-- End of Project Report Card -->
                 </div>
             </div>
         </div>
